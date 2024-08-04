a2d538b08c46acec1621ed2b8b7e836bef81a054
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-08-04 06:43:19 +0300

    initial

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = React.useState(false);
+  const toggleCart =() => {
+    setInCart(!inCart);
+  }
   return (
-    <li className="">
+    <li className= {inCart ? "true" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
       <button className="add">Add to Cart</button>
